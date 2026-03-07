d48591bfa75bac328d4ee33f4549bc8df79ed25a
eemhasanerol <eem.hasanerol@gmail.com>
2026-03-07 23:14:23 +0300

    Update bme280.c

--- Src/bme280.c
+++ Src/bme280.c
@@ -70,7 +70,13 @@ bme280_status_t bme280_init(bme280_dev_t *dev)
 }
 
 
-/* ---- BME280_LoadCalibration  ---- */
+
+/**
+ * @brief Load calibration coefficients into dev->calib.
+ * @param[in] dev  Device handle
+ * @retval STATUS_OK     Success
+ * @retval STATUS_ERROR  Comm or null pointer
+ */
 bme280_status_t bme280_load_calibration(bme280_dev_t *dev)
 {
     if (!dev) {
